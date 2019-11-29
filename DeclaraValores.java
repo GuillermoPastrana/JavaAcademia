01cf873b6d5a63914de509a0e3c1eef93ddf548e
GuillermoPastrana <57964574+GuillermoPastrana@users.noreply.github.com>
2019-11-29 14:06:28 -0600

    Add files via upload

---
+++ DeclaraValores.java
@@ -0,0 +1,11 @@
+//Ejercicio 2
+public class DeclaraValores{
+	public static void main(String[]args)
+	{
+		String uno = "Hi";
+		String dos = "Bye";
+		
+		System.out.println(uno+dos+dos+uno);
+	}
+	
+}
